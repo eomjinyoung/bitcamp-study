16b89e9844ec83c74011e8e081223b4aca93bc88
eomjinyoung <jinyoung.eom@gmail.com>
2025-03-25 10:27:16 +0900

    .

--- myproject/app/README.md
+++ myproject/app/README.md
@@ -1,52 +1,12 @@
-# 12. 스프링 부트 기반 설정으로 최적화 수행
+# 13. 기능 별로 패키지를 나누기
 
 ## 학습목표
 
-- application.properties의 다양한 속성의 역할을 이해한다.
-- Spring WebMVC 설정에 맞춰 페이지 컨트롤러를 최적화 할 수 있다.
+- 기능 별로 패키지를 만들어 MVC 컴포넌트를 배치할 수 있다.
+- Mybatis의 @Mapper 애노테이션을 사용할 수 있다.
 
 ## 작업
 
 ### 1. AWS S3 API 버전 1을 사용할 때 발생하는 경고 메시지 제거
 
 - NCPObjectStorageService 변경
-  
-### 2. Mybatis 설정 최적화
-
-- application.properties 변경
-```properties
-spring.datasource.url=jdbc:mysql://db-32e40j-kr.vpc-pub-cdb.ntruss.com:3306/studentdb
-spring.datasource.username=student
-spring.datasource.password=bitcamp123!@#
-spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
-
-mybatis.mapper-locations=classpath:bitcamp/myapp/mapper/*Dao.xml
-mybatis.type-aliases-package=bitcamp.myapp.vo
-```
-
-- mybatis.mapper-locations 프로퍼티 설정을 생략
-  - SQL 매퍼 파일을 인터페이스의 패키지 경로 및 이름과 같게 한다.
-
-### 3. JSP를 외부 노출에서 감추기
-
-- `/WEB-INF/view` 폴더 아래로 JSP 파일을 옮긴다.
-- application.properties 변경
-```properties
-spring.mvc.view.prefix=/WEB-INF/view/
-spring.mvc.view.suffix=.jsp
-```
-
-### 4. 페이지 컨트롤러 최적화
-
-- @RequestMapping 교체
-  - @GetMapping, @PostMapping 으로 교체
-  - 클래스에 기본 URL 지정하고 메서드에는 나머지 URL 설정
-- 클라이언트가 보낸 요청 파라미터 값을 도메인 객체로 직접 받기
-  - BoardController의 add(), update()에서 Board 타입 파라미터 사용하기
-- request handler의 파라미터 정리
-  - 메서드에서 사용하지 않는 파라미터 제거
-  - 프론트 컨트롤러에서 직접 받을 수 있는 것은 직접 받기 
-  - 첨부 파일을 받을 때 Part 와 MultipartFile 로 받기 
-- request handler의 리턴 값 정리
-  - prefix, suffix를 고려하여 JSP 뷰 이름을 리턴하기
-  - 리턴 타입을 void로 설정하면 요청 URL을 view name으로 사용한다.
--- myproject/app/src/main/java/bitcamp/myapp/App.java
+++ myproject/app/src/main/java/bitcamp/myapp/App.java
@@ -1,14 +1,23 @@
 package bitcamp.myapp;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication // Gradle에서 bootRun 태스크를 수행할 때 실행시킬 메인 클래스를 지정하는 애노테이션
 @PropertySource("file:${user.home}/config/bitcamp-study.properties")
@@ -19,8 +28,40 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @MapperScan("bitcamp.myapp.dao")
 public class App {
 
+  @Value("${jdbc.driver}")
+  private String driver;
+
+  @Value("${jdbc.url}")
+  private String url;
+
+  @Value("${jdbc.username}")
+  private String username;
+
+  @Value("${jdbc.password}")
+  private String password;
+
+  // 스프링부트의 프로퍼티 값(application.properties)을 커스터마이징 할 때 사용하는 객체
+  private final ConfigurableEnvironment configurableEnvironment;
+
+  public App(ConfigurableEnvironment configurableEnvironment) {
+    this.configurableEnvironment = configurableEnvironment;
+  }
+
+  @PostConstruct // 생성자 호출 후에 스프링 IoC 컨테이너가 호출하는 메서드
+  public void init() throws Exception {
+    HashMap<String,Object> datasourceProperties = new HashMap<>();
+    datasourceProperties.put("spring.datasource.driver-class-name", driver);
+    datasourceProperties.put("spring.datasource.url", url);
+    datasourceProperties.put("spring.datasource.username", username);
+    datasourceProperties.put("spring.datasource.password", password);
+
+    MapPropertySource propertySource = new MapPropertySource("dynamicProperties", datasourceProperties);
+    configurableEnvironment.getPropertySources().addFirst(propertySource);
+  }
+
   public static void main(String[] args) {
     System.out.println("App 실행!");
     SpringApplication.run(App.class, args);
   }
+
 }
