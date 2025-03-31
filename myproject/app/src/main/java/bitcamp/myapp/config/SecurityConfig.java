e6603983d8cef2cb858a18323ba4b69b1a065170
eomjinyoung <jinyoung.eom@gmail.com>
2025-03-31 09:30:53 +0900

    .

--- myproject/app/src/main/java/bitcamp/myapp/config/SecurityConfig.java
+++ myproject/app/src/main/java/bitcamp/myapp/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 // 학습 목표:
