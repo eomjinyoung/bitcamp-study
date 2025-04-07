d9ed6b20c74ad10cdde4c382732dde2d1796c8bf
eomjinyoung <jinyoung.eom@gmail.com>
2025-04-07 13:08:32 +0900

    .

--- react/chapter-05/myapp/src/index.js
+++ react/chapter-05/myapp/src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import ingredients from "../data/ingredients-data";
+import App from "./components/App";
 
-ReactDOM.render(React.createElement("h1", null, "제목입니다!"), document.getElementById("root"));
+function clicked(name, content) {
+  alert(`${name}님 ${content}!`);
+}
+
+render(
+  <App content='안녕!! 리액트!' name='홍길동' onClick={clicked} />,
+  document.getElementById("root")
+);
