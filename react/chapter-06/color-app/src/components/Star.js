9b856db7abae43157b4211b59e9622f969f3e47f
eomjinyoung <jinyoung.eom@gmail.com>
2025-04-08 13:16:56 +0900

    .

--- react/chapter-06/color-app/src/components/Star.js
+++ react/chapter-06/color-app/src/components/Star.js
@@ -2,5 +2,6 @@ import React from "react";
 import { FaStar } from "react-icons/fa";
 
 export default function Star({ selected }) {
+  console.log(selected);
   return <FaStar color={selected ? "red" : "gray"} />;
 }
--- react/chapter-06/color-app/src/components/StarRating.js
+++ react/chapter-06/color-app/src/components/StarRating.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Star from "./Star";
 
 export default function StarRating({ totalStars = 5 }) {
-  //const stars = Array(totalStars).fill(undefined);
-  // const stars = [...Array(totalStars)];
-  // const list = stars.map((value) => <Star selected={true} />);
-  // return list;
-  return [...Array(totalStars)].map((v, i) => <Star key={i} selected={true} />);
+  const [x, f] = useState(3);
+  return [...Array(totalStars)].map((v, i) => {
+    return <Star key={i} selected={i < x} />;
+  });
 }
